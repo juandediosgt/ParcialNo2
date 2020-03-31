2a433ddfacc80ce637e74d512cfee4529f3b5038
juandediosgt <50521468+juandediosgt@users.noreply.github.com>
2020-03-31 14:02:39 -0600

    Inicio del proyecto

---
+++ src/parcialno2/ParcialNo2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialno2;
+
+/**
+ *
+ * @author Juan de Dios
+ */
+public class ParcialNo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main hola = new Main();
+        hola.setVisible(true);
+        
+    }
+    
+}
